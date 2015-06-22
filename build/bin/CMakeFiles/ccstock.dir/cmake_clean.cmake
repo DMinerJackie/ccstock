491493967360add07bd78f8f92f3933e859de769
steve.zhong <zhongsizhi@foxmail.com>
2015-06-22 11:25:19 +0800

    add build directory

---
+++ build/bin/CMakeFiles/ccstock.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ccstock.dir/ccstock.cpp.o"
+  "ccstock.pdb"
+  "ccstock"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ccstock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
